d3a1800f166cf4c91c9ca98d7c51a6807afb23d2
Daniel Küttel <kuettel@maily.org>
2026-05-30 20:53:00 +0200

    Update test_members.sql

--- tests/cases/test_members.sql
+++ tests/cases/test_members.sql
@@ -1,13 +1,16 @@
-INSERT INTO people (id, surname, name) VALUES (2, 'Huber', 'Anna');
+INSERT INTO people (surname, name) VALUES ('Huber', 'Anna');
 INSERT INTO clubs (ssv, name) VALUES ('200', 'Club A');
-INSERT INTO person_roles (id, dictionary_id) VALUES (1, 1);
-INSERT INTO competitive_groups (id, dictionary_id) VALUES (4, 1);
+INSERT INTO person_roles (dictionary_id) VALUES (1);
+INSERT INTO competitive_groups (dictionary_id) VALUES (1);
 
 INSERT INTO members (person_id, club_id, role_id, group_id)
-VALUES (2, 1, 1, 4);
+VALUES ((SELECT MAX(id) from people), 
+	(SELECT MAX(id) from clubs), 
+	(SELECT MAX(id) from person_roles),
+	(SELECT MAX(id) from competitive_groups));
 
 UPDATE members SET role_id=1 WHERE id=1;
 
-DELETE FROM members WHERE id=1;
+DELETE FROM members WHERE id=(SELECT MAX(id) from members);
 
 SELECT * FROM audit_log WHERE table_name='members';
