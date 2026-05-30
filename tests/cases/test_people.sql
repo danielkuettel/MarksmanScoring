a3d2dfcf1e29ba69a5a38aedd0364cb5e9e633bc
Daniel Küttel <kuettel@maily.org>
2026-05-30 21:44:46 +0200

    Update test_people.sql

--- tests/cases/test_people.sql
+++ tests/cases/test_people.sql
@@ -1,9 +1,7 @@
-SELECT * FROM people;
+INSERT INTO people (surname, name, ssv, birth_date)
+VALUES ('Muster', 'Max', 12345, '1980-01-01');
 
-INSERT INTO people (id, surname, name, ssv, birth_date)
-VALUES (1, 'Muster', 'Max', 12345, '1980-01-01');
-
-DELETE FROM people WHERE id=1;
+DELETE FROM people WHERE id = (SELECT MAX(id) FROM people);
 
 SELECT old_values FROM audit_log
 WHERE table_name='people' AND action='DELETE';
