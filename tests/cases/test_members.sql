9cc327bfb38f322352b332fa7da7cdec82fea4e0
Daniel Küttel <kuettel@maily.org>
2026-05-30 21:13:46 +0200

    fixing test_members

--- tests/cases/test_members.sql
+++ tests/cases/test_members.sql
@@ -1,7 +1,17 @@
 INSERT INTO people (surname, name) VALUES ('Huber', 'Anna');
 INSERT INTO clubs (ssv, name) VALUES ('200', 'Club A');
-INSERT INTO person_roles (dictionary_id) VALUES (1);
-INSERT INTO competitive_groups (dictionary_id) VALUES (1);
+
+-- Insert person_roles
+INSERT INTO dictionaries (reference) VALUES ('person_roles');
+INSERT INTO dictionary_languages (dictionary_id, lang, name)
+VALUES ((SELECT MAX(id) FROM dictionaries), 'de', 'Rolen-Test');
+INSERT INTO person_roles (dictionary_id) SELECT MAX(id) FROM dictionaries;
+
+-- Insert competitive_groups
+INSERT INTO dictionaries (reference) VALUES ('person_roles');
+INSERT INTO dictionary_languages (dictionary_id, lang, name)
+VALUES ((SELECT MAX(id) FROM dictionaries), 'de', 'Gruppe-Test');
+INSERT INTO competitive_groups (dictionary_id) SELECT MAX(id) FROM dictionaries;
 
 INSERT INTO members (person_id, club_id, role_id, group_id)
 VALUES ((SELECT MAX(id) from people), 
