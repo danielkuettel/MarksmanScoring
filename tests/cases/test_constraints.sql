580c9d1f5e7d07a920d9c2851669c04406a47e6d
Daniel Küttel <kuettel@maily.org>
2026-05-30 18:31:03 +0200

    fixes constraint test

--- tests/cases/test_constraints.sql
+++ tests/cases/test_constraints.sql
@@ -1,2 +1,8 @@
--- dictionary_id is NOT NULL and UNIQUE
-INSERT INTO categories (id, dictionary_id) VALUES (20, NULL);  -- expected fail
+-- dictionary_id is NOT NULL ? this should be ignored
+INSERT OR IGNORE INTO categories (id, dictionary_id)
+VALUES (20, NULL);
+
+-- verify that row was NOT inserted
+SELECT COUNT(*) AS should_be_zero
+FROM categories
+WHERE id = 20;
